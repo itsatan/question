ba8ed0e73a3b66fabb763c43e1f4b11f35a11ac0
v_zhujiaxing01 <v_zhujiaxing01@baidu.com>
2023-04-12 23:09:01 +0800

    feat: 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { SmileOutlined } from '@ant-design/icons'
+import { Button, Result } from 'antd'
+import { useNavigate } from 'react-router-dom'
+import { MANAGE_INDEX_PATHNAME } from '../router'
+import styles from './NotFound.module.scss'
 
 const NotFound: React.FC = () => {
-	return <div>NotFound</div>
+	const navigate = useNavigate()
+	return (
+		<div className={styles.container}>
+			<Result
+				icon={<SmileOutlined />}
+				title="抱歉，您访问的页面不存在"
+				extra={
+					<Button type="primary" onClick={() => navigate(MANAGE_INDEX_PATHNAME)}>
+						返回首页
+					</Button>
+				}
+			/>
+		</div>
+	)
 }
 
 export default NotFound
