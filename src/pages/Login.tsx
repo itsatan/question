469fb4bb58882f6545c311159709ccb59b598397
v_zhujiaxing01 <v_zhujiaxing01@baidu.com>
2023-04-13 23:43:16 +0800

    feat: login and register

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,89 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import styles from './Login.module.scss'
+import { Space, Typography, Button, Form, Input, Checkbox } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import { REGISTER_PATHNAME } from '../router'
+
+const { Title } = Typography
+
+interface LoginProps {
+	username: string
+	password: string
+	remember: boolean
+}
+
+const USERNAME_KEY = 'USERNAME'
+const PASSWORD_KEY = 'PASSWORD'
+
+const rememberUser = (username: string, password: string) => {
+	localStorage.setItem(USERNAME_KEY, username)
+	localStorage.setItem(PASSWORD_KEY, password)
+}
+
+const deleteUserFromStorage = () => {
+	localStorage.removeItem(USERNAME_KEY)
+	localStorage.removeItem(PASSWORD_KEY)
+}
+
+const getUserFromStorage = () => {
+	return {
+		username: localStorage.getItem(USERNAME_KEY),
+		password: localStorage.getItem(PASSWORD_KEY),
+	}
+}
 
 const Login: React.FC = () => {
-	return <div>Login</div>
+	const [form] = Form.useForm()
+	useEffect(() => {
+		const { username, password } = getUserFromStorage()
+		form.setFieldsValue({ username, password })
+	}, [])
+	const onFinish = (params: LoginProps) => {
+		const { username, password, remember } = params
+		if (remember) {
+			rememberUser(username, password)
+		} else {
+			deleteUserFromStorage()
+		}
+		console.log(params)
+	}
+	return (
+		<div className={styles.container}>
+			<Space>
+				<Title level={2}>
+					<UserAddOutlined />
+				</Title>
+				<Title level={2}>用户登录</Title>
+			</Space>
+			<Form
+				labelCol={{ span: 6 }}
+				wrapperCol={{ span: 16 }}
+				onFinish={onFinish}
+				autoComplete="off"
+				initialValues={{ remember: true }}
+				form={form}
+			>
+				<Form.Item label="用户名" name="username">
+					<Input placeholder="请输入用户名" />
+				</Form.Item>
+				<Form.Item label="密码" name="password">
+					<Input.Password placeholder="请输入密码" />
+				</Form.Item>
+				<Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 6, span: 16 }}>
+					<Checkbox>记住我</Checkbox>
+				</Form.Item>
+				<Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+					<Space>
+						<Button type="primary" htmlType="submit">
+							登陆
+						</Button>
+						<Link to={REGISTER_PATHNAME}>注册新用户</Link>
+					</Space>
+				</Form.Item>
+			</Form>
+		</div>
+	)
 }
 
 export default Login
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,55 @@
 import React from 'react'
+import { Space, Typography, Button, Form, Input } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import styles from './Register.module.scss'
+import { Link } from 'react-router-dom'
+import { LOGIN_PATHNAME } from '../router'
+
+const { Title } = Typography
+
+interface RegisterProps {
+	username: string
+	password: string
+	confirm: string
+	nickname: string
+}
 
 const Register: React.FC = () => {
-	return <div>Register</div>
+	const onFinish = (params: RegisterProps) => {
+		console.log('提交', params)
+	}
+	return (
+		<div className={styles.container}>
+			<Space>
+				<Title level={2}>
+					<UserAddOutlined />
+				</Title>
+				<Title level={2}>注册新用户</Title>
+			</Space>
+			<Form labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} onFinish={onFinish} autoComplete="off">
+				<Form.Item label="用户名" name="username">
+					<Input placeholder="请输入用户名" />
+				</Form.Item>
+				<Form.Item label="密码" name="password">
+					<Input.Password placeholder="请输入密码" />
+				</Form.Item>
+				<Form.Item label="确认密码" name="confirm">
+					<Input.Password placeholder="请确认密码" />
+				</Form.Item>
+				<Form.Item label="昵称" name="nickname">
+					<Input placeholder="请输入昵称" />
+				</Form.Item>
+				<Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+					<Space>
+						<Button type="primary" htmlType="submit">
+							注册
+						</Button>
+						<Link to={LOGIN_PATHNAME}>已有账户，登录</Link>
+					</Space>
+				</Form.Item>
+			</Form>
+		</div>
+	)
 }
 
 export default Register
