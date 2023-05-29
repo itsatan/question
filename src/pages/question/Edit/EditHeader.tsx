f282426d09814089ec60f903152c212699bfb9e0
v_zhujiaxing01 <v_zhujiaxing01@baidu.com>
2023-05-29 18:48:58 +0800

    feat: 修改网页标题

--- src/pages/question/Edit/EditHeader.tsx
+++ src/pages/question/Edit/EditHeader.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { ChangeEvent, useRef, useState } from 'react'
 import styles from './EditHeader.module.scss'
-import { Button, Space, Typography } from 'antd'
-import { LeftOutlined } from '@ant-design/icons'
+import { Button, Input, InputRef, Space, Typography } from 'antd'
+import { EditOutlined, LeftOutlined } from '@ant-design/icons'
 import { useNavigate } from 'react-router-dom'
 import EditToolbar from './EditToolbar'
+import useGetPageInfo from '../../../hooks/useGetPageInfo'
+import { useDispatch } from 'react-redux'
+import { changePageTitle } from '../../../store/pageInfoReducer'
 
 const { Title } = Typography
+
+// 显示和修改标题
+const TitleElem = () => {
+	const { title } = useGetPageInfo()
+	const inputRef = useRef<InputRef>(null)
+	const dispatch = useDispatch()
+	const [editState, setEditState] = useState(false)
+	const handleClick = async () => {
+		await setEditState(true)
+		inputRef.current && inputRef.current.focus()
+	}
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const newTitle = event.target.value.trim()
+		if (!newTitle) return
+		dispatch(changePageTitle(newTitle))
+	}
+	if (editState) {
+		return (
+			<Input
+				ref={inputRef}
+				value={title}
+				onBlur={() => setEditState(false)}
+				onPressEnter={() => setEditState(false)}
+				onChange={handleChange}
+			/>
+		)
+	}
+	return (
+		<Space>
+			<Title level={5}>{title}</Title>
+			<Button type="text" icon={<EditOutlined />} onClick={handleClick} />
+		</Space>
+	)
+}
 
 const EditHeader: React.FC = () => {
 	const navigate = useNavigate()
@@ -17,7 +54,7 @@ const EditHeader: React.FC = () => {
 						<Button type="link" icon={<LeftOutlined />} onClick={() => navigate(-1)}>
 							返回
 						</Button>
-						<Title level={5}>问卷标题</Title>
+						<TitleElem />
 					</Space>
 				</div>
 				<div className={styles.main}>
--- src/store/pageInfoReducer.ts
+++ src/store/pageInfoReducer.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import produce from 'immer'
 
 export type PageInfoType = {
 	title: string
@@ -22,8 +23,12 @@ const pageInfoSlice = createSlice({
 		resetPageInfo: (state: PageInfoType, action: PayloadAction<PageInfoType>) => {
 			return action.payload
 		},
+		// 更新网页标题
+		changePageTitle: produce((draft: PageInfoType, action: PayloadAction<string>) => {
+			draft.title = action.payload
+		}),
 	},
 })
 
-export const { resetPageInfo } = pageInfoSlice.actions
+export const { resetPageInfo, changePageTitle } = pageInfoSlice.actions
 export default pageInfoSlice.reducer
